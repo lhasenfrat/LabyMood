5ef4a163bddb5dc5f03998d6eaf58a8d86c7fe5d
lhasenfrat <louis.hasenfratz@gmail.com>
2022-09-26 16:33:42 -0400

    add script labyrinth

---
+++ Assets/generationLabyrinthe.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generationLabyrinthe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("generation de terrain");
+    }
+
+    
+}
